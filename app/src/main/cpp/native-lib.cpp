268394effb77318bb26b1dcb28e8871b0c934a94
Andres Missiego <andres.missiego@edu.uah.es>
2024-12-13 18:52:16 +0100

    JNIWrapper added and configured

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_secmes_secmessaging_MainActivity_stringFromJNI(
+Java_com_secmes_secmessaging_JNIWrapper_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
--- app/src/main/java/com/secmes/secmessaging/MainActivity.java
+++ app/src/main/java/com/secmes/secmessaging/MainActivity.java
@@ -9,28 +9,10 @@ import com.secmes.secmessaging.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'secmessaging' library on application startup.
-    static {
-        System.loadLibrary("secmessaging");
-    }
-
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        setContentView(R.layout.activity_main);
     }
-
-    /**
-     * A native method that is implemented by the 'secmessaging' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
